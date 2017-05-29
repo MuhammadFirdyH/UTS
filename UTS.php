ddd24c126cac7d8bedc32d1f5ad7974e1cba109a
MuhammadFirdyH <hardyansyahf@gmail.com>
2017-05-29 13:09:17 +0700

    Add files via upload

---
+++ UTS.php
@@ -0,0 +1,36 @@
+<html>
+<body>
+<center>
+ <div id="header"><h1>PENDAFTARAN PELANGGAN BARU</h1></div>
+ <tr>
+ <td colspan="2"
+ id="Data Pribadi"><h2>Data Pribadi</h2></td>
+ </tr>
+<form action ="proses.php" method="get">
+<pre>
+Nama Lengkap		: <input type="text" name="nama" />
+Jenis Kelamin		: <input type="text" name="JK" />
+Email			: <input type="text" name="email" />
+<tr>
+ <td colspan="2"
+ id="Data Alamat Pengiriman"><h2>Data Alamat Pengiriman</h2></td>
+ </tr>
+Nama Negara		: <input type="text" name="nama" />
+Kota			: <input type="text" name="kota" />
+Alamat			: <input type="text" name="alamat" />
+Kode Post		: <input type="text" name="kode" />
+<tr>
+ <td colspan="2"
+ id="Data No Kontak"><h2>Data No Kontak</h2></td>
+ </tr>
+No Telepon		: <input type="text" name="telpon" />
+No Handphone		: <input type="text" name="hp" />
+No Faksimili		: <input type="text" name="faks" />
+
+		<input type="submit" value="Submit">
+		<input type="reset" value="Reset">
+        <input type="text">
+</pre>
+</form>
+</body>
+</html>
---
+++ proses.php
@@ -0,0 +1,21 @@
+<?php
+	echo $_GET['nama'];
+	echo "<br/>";
+	echo $_GET['JK'];
+	echo "<br/>";
+	echo $_GET['email'];
+	echo "<br/>";
+	echo $_GET['nama'];
+	echo "<br/>";
+	echo $_GET['kota'];
+	echo "<br/>";
+	echo $_GET['alamat'];
+	echo "<br/>";
+	echo $_GET['kode'];
+	echo "<br/>";
+	echo $_GET['telpon'];
+	echo "<br/>";
+	echo $_GET['hp'];
+	echo "<br/>";
+	echo $_GET['faks'];
+?>
